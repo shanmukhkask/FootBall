89b66e79b3c66449bcda1e09472a6fbf92dcac9d
shanmukh.kask <shanmukh.kask@gmail.com>
2018-10-17 18:15:25 +0530

    Intents

---
+++ app/src/main/java/example/com/football/Constants.kt
@@ -0,0 +1,6 @@
+package example.com.football
+
+/**
+ * Created by home on 17-Oct-18.
+ */
+const val EXTRA_LEAGUE="league"
--- app/src/main/java/example/com/football/LeagueActivity.kt
+++ app/src/main/java/example/com/football/LeagueActivity.kt
@@ -3,15 +3,47 @@ package example.com.football
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
+    var selectedLeague=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
     fun leagueNextClick(view:View)
     {
-        startActivity(Intent(this,SkillActivity::class.java))
+        if (selectedLeague!="")
+        {
+            var intent=(Intent(this,SkillActivity::class.java))
+            intent.putExtra(EXTRA_LEAGUE,selectedLeague)
+            startActivity(intent)
+        }
+        else
+        {
+            Toast.makeText(this,"No one is selected",Toast.LENGTH_SHORT).show()
+        }
+
     }
+    fun menOnClick(view: View)
+    {
+        womenBtn.isChecked=false
+        coEdBtn.isChecked=false
+        selectedLeague="mens"
+    }
+    fun womenOnclick(view: View)
+    {
+        menBtn.isChecked=false
+        coEdBtn.isChecked=false
+        selectedLeague="womens"
+    }
+    fun coEdOnClick(view: View)
+    {
+        womenBtn.isChecked=false
+        menBtn.isChecked=false
+        selectedLeague="Co-Ed"
+    }
+
 }
